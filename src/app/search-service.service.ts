efc5c6d306adc74470a550d980b0eb09baaa6ef1
Tomas <ts@medei.dk>
2021-02-08 05:19:20 +0100

    Page upload

--- src/app/search-service.service.ts
+++ src/app/search-service.service.ts
@@ -16,7 +16,7 @@ export class SearchServiceService {
   }
 
   searchUsers(res: string) {
-    this.http.get<User[]>('https://seca-user-table.herokuapp.com/', { params: { search: res } })
+    this.http.get<User[]>('/', { params: { search: res } })
       .subscribe(response => {
       });
   }
